b3ae3be289239665407c1c50991877d4bd08ab75
luxiangrong <luxiangrongwave@gmail.com>
2015-03-17 15:54:33 +0800

    完成垂直方向进度条

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,7 +1,9 @@
 'use strict';
 var myChart = {};
 
-/* 
+/**
+ * 对象属性赋值
+ *
  * @param {Object} target 目标对象。
  * @param {Object} source 源对象。
  * @param {boolean} deep 是否复制(继承)对象中的对象。
@@ -50,19 +52,20 @@ myChart.progressBar = function(selector, config) {
             bottom: 5,
             left: 5
         },
-        itemHeight: 5
+        itemHeight: 5,
+        strokeWidth: 1
     };
 
     config = myChart.extend(defaults, config);
 
-
     var width = config.width,
         height = config.height,
-        itemHeight = config.itemHeight + 2,
+        itemHeight = config.itemHeight,
+        itemOuterHeight = itemHeight + config.strokeWidth * 2,
         itemWidth = width - config.padding.left - config.padding.right,
         svg,
         max,
-        itemCount = Math.ceil(height / itemHeight);
+        itemCount = Math.floor(height / itemOuterHeight);
 
     var scale = d3.scale.linear()
         .domain([0, itemCount])
@@ -72,14 +75,44 @@ myChart.progressBar = function(selector, config) {
         .domain([0, itemCount])
         .range(['#00a8ff', '#00ff80']);
 
+    /**
+     * 生成进度条的条状元素
+     * @return {void}
+     */
+    var itemBarGenerator = function(selection) {
+        selection.attr('width', itemWidth)
+            .attr('height', itemHeight)
+            .attr('rx', 3)
+            .attr('ry', 3)
+            .attr('transform', function(d) {
+                return 'translate(0,' + (scale(d) - itemOuterHeight) + ')';
+            });
+
+        return selection;
+    }
+
+    /**
+     * 生成进度条的过渡效果
+     * @param  {transition} transition d3的transition对象
+     * @return {transition}            返回d3的transition对象
+     */
+    var transitionGenerator = function(transition) {
+        transition.delay(function(d, i) {
+                return i * 50
+            })
+            .style('fill', function(d) {
+                return colorScale(d);
+            });
+
+        return transition;
+    }
+
     function progressBar(data) {
         max = data.max;
 
-
-
-        var progressData = d3.range(0, Math.round(itemCount * data.progress / data.max), 1);
-        var restData = d3.range(Math.round(itemCount * data.progress / data.max), itemCount, 1);
-
+        var progressData = d3.range(0, Math.floor(itemCount * data.progress / data.max), 1);
+        var restData = d3.range(Math.floor(itemCount * data.progress / data.max), itemCount, 1);
+        var bgData = d3.range(0, itemCount, 1);
 
         svg = d3.select(selector)
             .append('svg')
@@ -89,57 +122,57 @@ myChart.progressBar = function(selector, config) {
             .append('g')
             .attr('transform', 'translate(' + config.padding.left + ',' + config.padding.top + ')');
 
-        svg.selectAll('rect.progress')
-            .data(progressData)
-            .enter()
-            .append('rect')
-            .transition()
-            .attr('class', 'progress')
-            .attr('width', itemWidth)
-            .attr('height', itemHeight - 2)
-            .attr('transform', function(d) {
-                return 'translate(0,' + (scale(d) - itemHeight) + ')';
-            })
-            .attr('rx', 3)
-            .attr('ry', 3)
-            .style('fill', function(d) {
-                return colorScale(d);
-            });
 
-        svg.selectAll('rect.rest')
-            .data(restData)
+        svg.selectAll('rect.bg')
+            .data(bgData)
             .enter()
             .append('rect')
-            .attr('class', 'rest')
-            .attr('width', itemWidth)
-            .attr('height', itemHeight - 2)
-            .attr('transform', function(d) {
-                return 'translate(0,' + (scale(d) - itemHeight) + ')';
-            })
-            .attr('rx', 3)
-            .attr('ry', 3);
+            .attr('class', 'bg')
+            .call(itemBarGenerator);
+
+        progressBar.update(data);
     }
 
     progressBar.update = function(data) {
-        var progressData = d3.range(0, Math.round(itemCount * data.progress / max), 1);
+        var progressData = d3.range(0, Math.floor(itemCount * data.progress / max), 1);
 
-        svg.selectAll('rect.progress')
-            .data(progressData)
-            .enter()
-            .append('rect')
-            .transition()
-            .attr('class', 'progress')
-            .attr('width', itemWidth)
-            .attr('height', itemHeight - 2)
-            .attr('rx', 3)
-            .attr('ry', 3)
-            .attr('transform', function(d) {
-                console.log(d);
-                return 'translate(0,' + (scale(d) - itemHeight) + ')';
+        var progressItemBar = svg.selectAll('rect.progress')
+            .data(progressData, function(d) {
+                return d;
             })
+
+        progressItemBar.call(itemBarGenerator);
+
+        var enterSize = progressItemBar.enter().size(),
+            duration = 500;
+
+        progressItemBar.enter()
+            .append('rect')
+            .attr('class', 'progress')
+            .call(itemBarGenerator)
             .style('fill', function(d) {
                 return colorScale(d);
-            });
+            })
+            .style("fill-opacity", 1e-6)
+            .transition()
+            .duration(duration)
+            .delay(function(d, i) {
+                return (i + 1) / progressData.length * duration;
+            })
+            .style("fill-opacity", 1);
+
+        var exitSize = progressItemBar.exit().size();
+
+        progressItemBar
+            .exit()
+            .style("fill-opacity", 1)
+            .transition()
+            .duration(duration)
+            .delay(function(d, i) {
+                return (exitSize - i + progressData.length - 1) / exitSize * duration;
+            })
+            .style("fill-opacity", 1e-6)
+            .remove();
     }
 
     return progressBar;
