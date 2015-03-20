11b5ca49f25188b32d77e372857c023729e80c47
luxiangrong <luxiangrongwave@gmail.com>
2015-03-20 17:04:30 +0800

    进度条增加方向选项

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -45,6 +45,7 @@ myChart.extend = function(target, /*optional*/ source, /*optional*/ deep) {
 };
 
 myChart.progressBar = function(selector, config) {
+    //默认参数
     var defaults = {
         padding: {
             top: 5,
@@ -52,28 +53,51 @@ myChart.progressBar = function(selector, config) {
             bottom: 5,
             left: 5
         },
-        itemHeight: 5,
-        strokeWidth: 1
+        itemSize: 5,
+        strokeWidth: 1,
+        direction: 'horizontal',
+        startColor: '#00a8ff',
+        endColor: '#00ff80',
+        duration: 500 //动画持续时间，调用update的间隔需要大于2倍duration
     };
 
     config = myChart.extend(defaults, config);
 
     var width = config.width,
         height = config.height,
-        itemHeight = config.itemHeight,
-        itemOuterHeight = itemHeight + config.strokeWidth * 2,
-        itemWidth = width - config.padding.left - config.padding.right,
+        itemWidth,
+        itemHeight,
+        itemOuterWidth,
+        itemOuterHeight,
         svg,
         max,
-        itemCount = Math.floor(height / itemOuterHeight);
+        itemCount;
 
+    if (config.direction === 'vertical') {
+        itemWidth = config.itemSize;
+        itemHeight = height - config.padding.top - config.padding.bottom;
+        itemOuterWidth = itemWidth + config.strokeWidth * 2;
+        itemCount = Math.floor((width - config.padding.left - config.padding.right) / itemOuterWidth);
+    } else {
+        itemWidth = width - config.padding.left - config.padding.right;
+        itemHeight = config.itemSize;
+        itemOuterHeight = itemHeight + config.strokeWidth * 2;
+        itemCount = Math.floor((height - config.padding.top - config.padding.bottom) / itemOuterHeight);
+    }
+
+    //用于定位小矩形位置的缩放函数
     var scale = d3.scale.linear()
-        .domain([0, itemCount])
-        .range([height - config.padding.bottom - config.padding.top, config.padding.top]);
+        .domain([0, itemCount - 1]);
+    if (config.direction === 'vertical') {
+        scale.range([0, width - config.padding.right - config.padding.left - itemWidth]);
+    } else {
+        scale.range([height - config.padding.bottom - config.padding.top - itemHeight, 0]);
+    }
 
+    //颜色缩放函数
     var colorScale = d3.scale.linear()
         .domain([0, itemCount])
-        .range(['#00a8ff', '#00ff80']);
+        .range([config.startColor, config.endColor]);
 
     /**
      * 生成进度条的条状元素
@@ -85,7 +109,11 @@ myChart.progressBar = function(selector, config) {
             .attr('rx', 3)
             .attr('ry', 3)
             .attr('transform', function(d) {
-                return 'translate(0,' + (scale(d) - itemOuterHeight) + ')';
+                if (config.direction === 'vertical') {
+                    return 'translate(' + (scale(d)) + ', 0)';
+                } else {
+                    return 'translate(0, ' + scale(d) + ')';
+                }
             });
 
         return selection;
@@ -107,34 +135,29 @@ myChart.progressBar = function(selector, config) {
         return transition;
     }
 
-    function progressBar(data) {
-        max = data.max;
+    var bgData = d3.range(0, itemCount, 1);
 
-        var progressData = d3.range(0, Math.floor(itemCount * data.progress / data.max), 1);
-        var restData = d3.range(Math.floor(itemCount * data.progress / data.max), itemCount, 1);
-        var bgData = d3.range(0, itemCount, 1);
+    //生成背景矩形块
+    svg = d3.select(selector)
+        .append('svg')
+        .attr('class', 'progress-bar')
+        .attr('width', width)
+        .attr('height', height)
+        .append('g')
+        .attr('transform', 'translate(' + config.padding.left + ',' + config.padding.top + ')');
+    svg.selectAll('rect.bg')
+        .data(bgData)
+        .enter()
+        .append('rect')
+        .attr('class', 'bg')
+        .call(itemBarGenerator);
 
-        svg = d3.select(selector)
-            .append('svg')
-            .attr('class', 'progress-bar')
-            .attr('width', width)
-            .attr('height', height)
-            .append('g')
-            .attr('transform', 'translate(' + config.padding.left + ',' + config.padding.top + ')');
+    //创建一个函数对象，可以在初始化后调用其他扩展方法
+    var progressBar = new Function();
 
-
-        svg.selectAll('rect.bg')
-            .data(bgData)
-            .enter()
-            .append('rect')
-            .attr('class', 'bg')
-            .call(itemBarGenerator);
-
-        progressBar.update(data);
-    }
-
+    //更新进度条的状态
     progressBar.update = function(data) {
-        var progressData = d3.range(0, Math.floor(itemCount * data.progress / max), 1);
+        var progressData = d3.range(0, Math.floor(itemCount * data.progress / config.max), 1);
 
         var progressItemBar = svg.selectAll('rect.progress')
             .data(progressData, function(d) {
@@ -144,7 +167,7 @@ myChart.progressBar = function(selector, config) {
         progressItemBar.call(itemBarGenerator);
 
         var enterSize = progressItemBar.enter().size(),
-            duration = 500;
+            duration = config.duration;
 
         progressItemBar.enter()
             .append('rect')
@@ -153,7 +176,7 @@ myChart.progressBar = function(selector, config) {
             .style('fill', function(d) {
                 return colorScale(d);
             })
-            .style("fill-opacity", 1e-6)
+            .style("fill-opacity", 0)
             .transition()
             .duration(duration)
             .delay(function(d, i) {
@@ -171,7 +194,7 @@ myChart.progressBar = function(selector, config) {
             .delay(function(d, i) {
                 return (exitSize - i + progressData.length - 1) / exitSize * duration;
             })
-            .style("fill-opacity", 1e-6)
+            .style("fill-opacity", 0)
             .remove();
     }
 
@@ -188,8 +211,8 @@ myChart.progressKnob = function(selector, config) {
     config = myChart.extend(defaults, config);
 
     var svg, radius = config.radius;
-    var sliceData = [];
 
+    var sliceData = [];
     for (var i = 0; i < config.intervalNum; i++) {
         sliceData.push(1);
     }
@@ -226,7 +249,7 @@ myChart.progressKnob = function(selector, config) {
         .attr("class", "slice");
 
     var progressSlice = svg.select(".slices").append('path')
-            .attr('class', 'progressSlice');
+        .attr('class', 'progressSlice');
 
     buttons.append('image')
         .attr('xlink:href', 'images/knob01.png')
@@ -236,9 +259,9 @@ myChart.progressKnob = function(selector, config) {
         .attr('y', 0);
 
     var progressDot = svg.append('image')
-            .attr('xlink:href', 'images/knob02.png')
-            .attr('width', 12)
-            .attr('height', 12);
+        .attr('xlink:href', 'images/knob02.png')
+        .attr('width', 12)
+        .attr('height', 12);
 
     var progressKnob = function() {};
     var lastProgress = 0;
@@ -248,16 +271,17 @@ myChart.progressKnob = function(selector, config) {
         var progressAngle = progress / config.max * 2 * Math.PI;
         var lastProgressAngle = lastProgress / config.max * 2 * Math.PI;
 
-        // var progressSlice = svg.select(".slices").selectAll('.progressSlice')
-        //     .data(pie([progress, config.max - progress]));
-
-
         progressSlice.transition()
             .duration(800)
-            // .attr('d', arcGenerator({startAngle:0, endAngle: progress / config.max * Math.PI * 2}))
             .attrTween("d", function(d) {
-                this._current = this._current || {startAngle: 0, endAngle: progressAngle};
-                var interpolate = d3.interpolateObject(this._current, {startAngle: 0, endAngle: progressAngle});
+                this._current = this._current || {
+                    startAngle: 0,
+                    endAngle: progressAngle
+                };
+                var interpolate = d3.interpolateObject(this._current, {
+                    startAngle: 0,
+                    endAngle: progressAngle
+                });
                 this._current = interpolate(0);
                 return function(t) {
                     return arcGenerator(interpolate(t));
@@ -266,17 +290,15 @@ myChart.progressKnob = function(selector, config) {
 
         progressDot.transition()
             .duration(800)
-            .attrTween('x', function(d){
+            .attrTween('x', function(d) {
                 this._current = this._current || progressAngle;
                 var interpolate = d3.interpolate(lastProgressAngle, progressAngle);
                 this._current = interpolate(0);
-                console.log('last: ' + lastProgressAngle);
-                console.log('current: ' + progressAngle);
                 return function(t) {
                     return (radius - 30) * Math.cos(interpolate(t) - Math.PI / 2) - 6;
                 };
             })
-            .attrTween('y', function(d){
+            .attrTween('y', function(d) {
                 this._current = this._current || progressAngle;
                 var interpolate = d3.interpolate(lastProgressAngle, progressAngle);
                 this._current = interpolate(0);
@@ -289,4 +311,42 @@ myChart.progressKnob = function(selector, config) {
     }
 
     return progressKnob;
+};
+
+// 简单仪表
+myChart.simpleGauge = function(selector, config) {
+    var defaults = {
+        width: 200,
+        height: 200,
+        tick: {
+            min: 0,         //最小刻度
+            max: 7,         //最大刻度
+            startDeg: 270,  //刻度起始角度
+            endDeg: 450,    //刻度结束角度
+            direction: 1    //刻度方向，1为顺时针，-1为逆时针
+        }
+    }
+    var config = myChart.extend(defaults, config);
+    var simpleGauge = new Function();
+
+    var svg, ticksContainer, radius;
+
+    radius = d3.min([config.width, config.height]) / 2;
+    svg = d3
+        .select(selector)
+        .append('g')
+        .attr('class', 'simple-gauge');
+
+    ticksContainer = svg.append('g')
+        .attr('class', 'ticks')
+        .attr('transform', 'translate(' + radius + ',' + radius + ')');
+
+    var buildTicks = function(){
+        
+    };
+
+    simpleGauge.update = function(value){
+
+    };
+    return simpleGauge;
 };
